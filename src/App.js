676b05aed17471c5a29c9d0402ebec702a335b28
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-29 12:35:11 +0100

    Add todosList state to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+const initialTodos = [
+  { id: '20041996', task: 'Pay the electric bill', complete: false },
+  { id: '20041997', task: 'Pay the electric bill', complete: true },
+  { id: '20041998', task: 'Pay the electric bill', complete: false },
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(props) {
+    super(props);
+    this.state = {
+      todosList: initialTodos,
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
